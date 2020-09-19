7d141a3489799ec3bea22d46473332ab66dd4383
julian-goettingen <jlagg97@gmail.com>
2020-09-19 02:33:05 +0200

    Spielzeug

--- main.py
+++ main.py
@@ -10,6 +10,34 @@ matplotlib.use('Qt5Agg')
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
 
+class WaveParams():
+
+    def __init__(self, offset, freqs, amps):
+
+        self.offset = offset
+        assert(freqs.shape == amps.shape)
+        self.freqs = freqs
+        self.amps = amps
+
+        self.any_change_callbacks = []
+
+    def register_any_change_callback(self, fun):
+
+        self.any_change_callbacks.append(fun)
+
+    def change_amp(self, new_amp, index):
+
+        self.amps[index] = new_amp
+        for f in self.any_change_callbacks:
+            f()
+
+    def change_offset(self, offset):
+
+        self.offset = offset
+        for f in self.any_change_callbacks:
+            f()
+
+
 class LissMainWindow(QMainWindow):
 
     def __init__(self):
@@ -20,20 +48,58 @@ class LissMainWindow(QMainWindow):
 
     def initUI(self):
 
-        self.firstFreq = freqSelector(np.linspace(400,600,10), "first freq\n(x-axis)", 0)
-        self.sndFreq = freqSelector(np.linspace(400,600,10), "snd freq\n(y-axis)", 90)
+        self.fullyInitialized = False
+
+        sz = 10
+        x_freqs = np.linspace(400,600,sz)
+        y_freqs = x_freqs.copy()
+        x_amps = np.array([90 if i==0 or i==2 or i==4 else 5 for i in range(sz)])
+        y_amps = np.array([80 if i==1 or i==5 else 0 for i in range(sz)])
+        x_offset = 0
+        y_offset = 90
+
+        self.x_wave = WaveParams(x_offset, x_freqs, x_amps)
+        self.y_wave = WaveParams(y_offset, y_freqs, y_amps)
+
+        self.x_wave.register_any_change_callback(self.replot_static)
+        self.y_wave.register_any_change_callback(self.replot_static)
+
+
+        self.firstFreqSelector = freqSelector(self.x_wave, "first freq\n(x-axis)")
+        self.sndFreqSelector = freqSelector(self.y_wave, "snd freq\n(y-axis)")
 
         lo = QVBoxLayout()
 
-        lo.addWidget(self.firstFreq)
-        lo.addWidget(self.sndFreq)
+        lo.addWidget(self.firstFreqSelector)
+        lo.addWidget(self.sndFreqSelector)
 
-        self.mainVis = mainVisual()
-        lo.addWidget(self.mainVis)
+        vis = QHBoxLayout()
+        self.dynVis = dynamicVisual()
+        vis.addWidget(self.dynVis)
+
+        self.staticVis = staticVisual([],[])
+        vis.addWidget(self.staticVis)
+
+        lo.addLayout(vis)
 
         central = QWidget()
         central.setLayout(lo)
         self.setCentralWidget(central)
+
+        self.fullyInitialized = True
+        self.replot_static()
+
+    def replot_static(self):
+
+        if not self.fullyInitialized:
+            return
+
+        t = np.linspace(0,0.1,500)
+        xdata = sum([a * np.sin(f * t + self.x_wave.offset) for a,f in zip(self.x_wave.amps, self.x_wave.freqs)])
+        ydata = sum([a * np.sin(f * t + self.y_wave.offset) for a,f in zip(self.y_wave.amps, self.y_wave.freqs)])
+
+
+        self.staticVis.replot(xdata, ydata)
 
     def run(self):
 
@@ -44,27 +110,42 @@ class LissMainWindow(QMainWindow):
 
     def plot_next(self):
 
-        x = self.firstFreq.generate_data()
-        y = self.sndFreq.generate_data()
+        x = self.firstFreqSelector.generate_data()
+        y = self.sndFreqSelector.generate_data()
 
-        self.mainVis.accept_new_data_point(x,y)
+        self.dynVis.accept_new_data_point(x, y)
 
-class mainVisual(FigureCanvasQTAgg):
+class staticVisual(FigureCanvasQTAgg):
 
-    def __init__(self):
-        self.fig = Figure((2, 2), 100)
+    def __init__(self, xdata, ydata):
+        self.fig = Figure((200,200), 100)
         super().__init__(self.fig)
         self.axes = self.fig.add_subplot(111)
-        # self.axes.set_xlim(-1,1)
-        # self.axes.set_ylim(-1,1)
         self.axes.set_aspect('equal')
 
-        self.maxdata = 300
+        self.replot(xdata, ydata)
+
+    def replot(self, xdata, ydata):
+
+        self.axes.clear()
+        self.axes.plot(xdata, ydata)
+        self.fig.canvas.draw()
+
+class dynamicVisual(FigureCanvasQTAgg):
+
+    def __init__(self):
+        self.fig = Figure((200, 200), 100)
+        super().__init__(self.fig)
+        self.axes = self.fig.add_subplot(111)
+        self.axes.set_aspect('equal')
+
+        self.maxdata = 100
         self.x_data = []
         self.y_data = []
 
     @pyqtSlot(int, int)
     def accept_new_data_point(self, x, y):
+
         self.x_data.append(x)
         self.y_data.append(y)
         if len(self.x_data) > self.maxdata:
@@ -79,13 +160,12 @@ class mainVisual(FigureCanvasQTAgg):
 
 class freqSelector(QWidget):
 
-    def __init__(self, freqlist, labeltext, initial_phase_offset):
+    def __init__(self, wave, labeltext):
 
         self.time = 0
         self.timestep = 0.0003
-        self.phase_offset = 0
-        self.amplitudes = np.zeros(freqlist.shape)
-        self.freqlist = freqlist
+
+        self.wave = wave
 
         super().__init__()
         lo = QHBoxLayout()
@@ -99,7 +179,8 @@ class freqSelector(QWidget):
         s.setMinimum(0); s.setMaximum(360)
         s.setSingleStep(5)
         s.valueChanged.connect(self.set_phase)
-        s.setValue(initial_phase_offset)
+        s.valueChanged.connect(wave.change_offset)
+        s.setValue(wave.offset)
         phase.addWidget(QLabel("phase offset"))
         phase.addWidget(s)
         info.addLayout(phase)
@@ -107,7 +188,7 @@ class freqSelector(QWidget):
         lo.addLayout(info)
 
 
-        for i, f in enumerate(freqlist):
+        for i, f in enumerate(wave.freqs):
             slide_lo = QVBoxLayout()
             s = QSlider()
             s.setMinimum(0)
@@ -117,7 +198,7 @@ class freqSelector(QWidget):
             l = QLabel(str(int(round(f,0))))
             act = partial(self.set_amplitude, freq_index=i)
             s.valueChanged.connect(act)
-            s.setValue(90 if i==0 or i==2 or i==4 else 5)
+            s.setValue(wave.amps[i])
             slide_lo.addWidget(l)
             lo.addLayout(slide_lo)
 
@@ -127,19 +208,19 @@ class freqSelector(QWidget):
 
     def set_amplitude(self, new_amp, freq_index):
 
-        self.amplitudes[freq_index] = new_amp
+        self.wave.change_amp(new_amp, freq_index)
 
     def set_phase(self, new_phase):
 
         assert(0 <= new_phase <= 360)
-        self.phase_offset = new_phase
+        self.wave.change_offset(new_phase)
 
 
     def generate_data(self):
 
         val = 0
-        for f,a in zip(self.freqlist, self.amplitudes):
-            val += a*np.sin(f*self.time+(self.phase_offset)*(2*np.pi/360))
+        for f,a in zip(self.wave.freqs, self.wave.amps):
+            val += a*np.sin(f*self.time+(self.wave.offset)*(2*np.pi/360))
 
         self.time += self.timestep
 
